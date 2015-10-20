f231d0dbf70668fbb68b421e87ef46f679eb0d5b
MATHIEU <benjamin.mathieu@capgemini.com>
2015-10-20 17:17:46 +0200

    ajout meta::math

--- src/core/meta.hpp
+++ src/core/meta.hpp
@@ -66,129 +66,152 @@ namespace brain
             /// std::integral_constant
             template < typename literal_t,
                      literal_t _l >
-            using igral = std::integral_constant<literal_t, _l>;
+            using igral =
+                std::integral_constant<literal_t, _l>;
 
 
             /// Wrapper for bool
             template<bool _b>
-            using bool_ = igral<bool, _b>;
+            using bool_ =
+                igral<bool, _b>;
 
 
             /// Wrapper for short
             template<short _s>
-            using short_ = igral<short, _s>;
+            using short_ =
+                igral<short, _s>;
 
 
             /// Wrapper for unsigned short
             template<unsigned short _us>
-            using ushort_ = igral<unsigned short, _us>;
+            using ushort_ =
+                igral<unsigned short, _us>;
 
 
             /// Wrapper for char
             template<char _c>
-            using char_ = igral<char, _c>;
+            using char_ =
+                igral<char, _c>;
 
 
             /// Wrapper for int
             template<int _i>
-            using int_ = igral<int, _i>;
+            using int_ =
+                igral<int, _i>;
 
 
             /// Wrapper for long
             template<long _l>
-            using long_ = igral<long, _l>;
+            using long_ =
+                igral<long, _l>;
 
 
             /// Wrapper for long long
             template<long long _ll>
-            using longlong_ = igral<long long, _ll>;
+            using longlong_ =
+                igral<long long, _ll>;
 
 
             /// Wrapper for unsigned
             template<unsigned _u>
-            using unsigned_ = igral<unsigned, _u>;
+            using unsigned_ =
+                igral<unsigned, _u>;
 
 
             /// Wrapper for unsigned long
             template<unsigned long _ul>
-            using unsignedl_ = igral<unsigned long, _ul>;
+            using unsignedl_ =
+                igral<unsigned long, _ul>;
 
 
             /// Wrapper for unsigned long long
             template<unsigned long long _ull>
-            using unsignedll_ = igral<unsigned long long, _ull>;
+            using unsignedll_ =
+                igral<unsigned long long, _ull>;
 
 
             /// Wrapper for size_t
             template<std::size_t _s>
-            using size_t_ = igral<std::size_t, _s>;
+            using size_t_ =
+                igral<std::size_t, _s>;
         }
 
 
         /// Shortcut for
         /// literal::bool_
         template<bool _b>
-        using bool_ = literal::bool_<_b>;
+        using bool_ =
+            literal::bool_<_b>;
 
 
         /// Shortcut for
         /// literal::short_
         template<short _s>
-        using short_ = literal::short_<_s>;
+        using short_ =
+            literal::short_<_s>;
 
 
         /// Shortcut for
         /// literal::ushort_
         template<unsigned short _us>
-        using ushort_ = literal::ushort_<_us>;
+        using ushort_ =
+            literal::ushort_<_us>;
 
 
         /// Shortcut for
         /// literal::char_
         template<char _c>
-        using char_ = literal::char_<_c>;
+        using char_ =
+            literal::char_<_c>;
 
 
         /// Shortcut for
         /// literal::int_
         template<int _i>
-        using int_ = literal::int_<_i>;
+        using int_ =
+            literal::int_<_i>;
 
 
         /// Shortcut for
         /// literal::long_
         template<long _l>
-        using long_ = literal::long_<_l>;
+        using long_ =
+            literal::long_<_l>;
 
 
         /// Shortcut for
         /// literal::longlong_
         template<long long _ll>
-        using longlong_ = literal::longlong_<_ll>;
+        using longlong_ =
+            literal::longlong_<_ll>;
 
 
         /// Shortcut for
         /// literal::unsigned_
         template<unsigned _u>
-        using unsigned_ = literal::unsigned_<_u>;
+        using unsigned_ =
+            literal::unsigned_<_u>;
 
 
         /// Shortcut for
         /// literal::unsignedl_
         template<unsigned long _ul>
-        using unsignedl_ = literal::unsignedl_<_ul>;
+        using unsignedl_ =
+            literal::unsignedl_<_ul>;
 
 
         /// Shortcut for
         /// literal::unsignedll_
         template<unsigned long long _ull>
-        using unsignedll_ = literal::unsignedll_<_ull>;
+        using unsignedll_ =
+            literal::unsignedll_<_ull>;
 
 
         /// Shortcut for
         /// literal::size_t_
         template<std::size_t _s>
-        using size_t_ = literal::size_t_<_s>;
+        using size_t_ =
+            literal::size_t_<_s>;
 
 
         /// ////////////////////////////// ///
@@ -198,36 +221,42 @@ namespace brain
         {
             /// Wrapper for sizeof
             template<typename type_t>
-            using sizeof_ = size_t_<sizeof(type_t)>;
+            using sizeof_ =
+                size_t_<sizeof(type_t)>;
 
 
             /// Wrapper for sizeof...
             template<typename ... types_t>
-            using sizeof_pack_ = size_t_<sizeof...(types_t)>;
+            using sizeof_pack_ =
+                size_t_<sizeof...(types_t)>;
 
 
             /// Wrapper for alignof
             template<typename type_t>
-            using alignof_ = size_t_<alignof(type_t)>;
+            using alignof_ =
+                size_t_<alignof(type_t)>;
         }
 
 
         /// Shortcut for
         /// constkw::sizeof_
         template<typename type_t>
-        using sizeof_ = constkw::sizeof_<type_t>;
+        using sizeof_ =
+            constkw::sizeof_<type_t>;
 
 
         /// Shortcut for
         /// constkw::sizeof_pack_
         template<typename ... types_t>
-        using sizeof_pack_ = constkw::sizeof_pack_<types_t...>;
+        using sizeof_pack_ =
+            constkw::sizeof_pack_<types_t...>;
 
 
         /// Shortcut for
         /// constkw::alignof_
         template<typename type_t>
-        using alignof_ = constkw::alignof_<type_t>;
+        using alignof_ =
+            constkw::alignof_<type_t>;
 
 
         /// //////////////////////////////////////// ///
@@ -241,7 +270,8 @@ namespace brain
             struct list
             {
                 using type = list;
-                static constexpr auto size = v_<sizeof_pack_<types_t...>>;
+                static constexpr auto size =
+                    v_<sizeof_pack_<types_t...>>;
             };
 
 
@@ -251,7 +281,7 @@ namespace brain
             struct front_;
 
 
-            /// Specialisation of 
+            /// Specialisation of
             /// front_ that identifies
             /// the head_t of list_t
             template < typename head_t,
@@ -260,11 +290,12 @@ namespace brain
             {
                 using type = head_t;
             };
-            
-            
+
+
             /// Evaluates t_<front_<list_t>>
             template<typename list_t>
-            using front = t_<front_<list_t>>;
+            using front =
+                t_<front_<list_t>>;
 
 
             /// Returns last type
@@ -286,13 +317,157 @@ namespace brain
 
             /// Evaluates t_<back_<list_t>>
             template<typename list_t>
-            using back = t_<back_<list_t>>;
+            using back =
+                t_<back_<list_t>>;
         }
 
 
         /// Shortuct for pack::list
         template<typename ... types_t>
-        using list = pack::list<types_t...>;
+        using list =
+            pack::list<types_t...>;
+
+
+        /// Shortcut for pack::front
+        template<typename list_t>
+        using front =
+            pack::front<list_t>;
+
+
+        /// Shortcut for pack::back
+        template<typename list_t>
+        using back =
+            pack::back<list_t>;
+
+
+        /// ///////////////////////////// ///
+        /// Mathematical wrapper features ///
+        /// ///////////////////////////// ///
+        namespace math
+        {// TODO Faire les docs des features math::
+            template<typename type_t>
+            using inc =
+                literal::igral <
+                decltype(v_<type_t> + 1),
+                v_<type_t> + 1 >;
+
+
+            template<typename type_t>
+            using inc =
+                literal::igral <
+                decltype(v_<type_t> - 1),
+                v_<type_t> - 1 >;
+
+
+            template < typename type_t,
+                     typename other_t >
+            using plus =
+                literal::igral <
+                decltype(v_<type_t> + v_<other_t>),
+                v_<type_t> + v_<other_t >>;
+
+
+            template < typename type_t,
+                     typename other_t >
+            using minus =
+                literal::igral <
+                decltype(v_<type_t> - v_<other_t>),
+                v_<type_t> - v_<other_t >>;
+
+
+            template < typename type_t,
+                     typename other_t >
+            using multiplies =
+                literal::igral <
+                decltype(v_<type_t> * v_<other_t>),
+                v_<type_t> * v_<other_t >>;
+
+
+            template < typename type_t,
+                     typename other_t >
+            using divides =
+                literal::igral <
+                decltype(v_<type_t> / v_<other_t>),
+                v_<type_t> / v_<other_t >>;
+
+
+            template < typename type_t,
+                     typename other_t >
+            using negate =
+                literal::igral <
+                decltype(-v_<type_t>),
+                -v_<type_t >>;
+
+
+            template < typename type_t,
+                     typename other_t >
+            using modulus =
+                literal::igral <
+                decltype(v_<type_t> % v_<other_t>),
+                v_<type_t> % v_<other_t >>;
+        }
+
+
+        template<typename type_t>
+        using inc =
+            math::inc<type_t>;
+
+
+        template<typename type_t>
+        using dec =
+            math::dec<type_t>;
+
+
+        template < typename type_t,
+                 typename other_t >
+        using plus =
+            math::plus <
+            type_t,
+            other_t >;
+
+
+        template < typename type_t,
+                 typename other_t >
+        using minus =
+            math::minus <
+            type_t,
+            other_t >;
+
+
+        template < typename type_t,
+                 typename other_t >
+        using multiplies =
+            math::multiplies <
+            type_t,
+            other_t >;
+
+
+        template < typename type_t,
+                 typename other_t >
+        using divides =
+            math::divides <
+            type_t,
+            other_t >;
+
+
+        template < typename type_t>
+        using negate =
+            math::negate<type_t>;
+
+
+        template < typename type_t,
+                 typename other_t >
+        using modulus =
+            math::modulus <
+            type_t,
+            other_t >;
+
+        /// /////////////////////// ///
+        /// Logical wrapper feature ///
+        /// /////////////////////// ///
+        namespace logic
+        {
+        }
     }
 }
 
